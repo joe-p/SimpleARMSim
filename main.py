574c762b193af31b48803d94023891fc389986bc
Polny <polnyj@wit.edu>
2019-07-17 14:34:55 -0400

    Created binary class to easily manipulate binary numbers

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
 
+class Binary:
+    def __init__(self, n, bits):
+        self.bin = '{:0{}b}'.format(n, bits)
+
+    def __str__(self):
+        return self.bin
+
+    def digit(self, n):
+        # Reverses so 0 is LSB, instead of MSB
+        return self.bin[::-1][n]
+
+    # digits() retuns the specified bit range
+    # Example (Binary(0x0F).digits(0,3)) will return 1111
+    def digits(self, start, end):
+        s=""
+        end += 1
+        size = end - start
+
+        for n in range(start, end):
+            s = (self.digit(n)) + s
+
+        return Binary(int(s,2), size)
 
 class ALU:
 
@@ -9,7 +31,7 @@ class ALU:
         self.out = 0
 
     def add(self):
-        out = in1+in2
+        self.out = self.in1 + self.in2
 
 class ARM:
 
@@ -30,20 +52,42 @@ class ARM:
     mem_alu = ALU()
 
     instruction_memory = {
-            0: 0x0000,
-            4: 0x0000
+            0: Binary(0, 32),
+            4: Binary(0xAFFFFFFF, 32)
             }
 
-    instruction = 0x0000
+    instruction = Binary(0, 32)
+
+    registers = {
+            "Read Register 1": 0,
+            "Read Register 2": 0,
+            "Write Register": 0,
+            "Write Data": 0,
+            "Read Data 1": 0,
+            "Read Data 2": 0
+            }
 
     def __init__(self):
-        pc_alu.in2 = 4 # Input to the PC ALU is always 4
+        self.pc_alu.in2 = 4 # Input to the PC ALU is always 4
 
     def instruction_fetch(self):
-        pc_alu.in1 = pc
-        pc_alu.add() # pc + 4
+        self.instruction = self.instruction_memory[self.pc] # Get the instruction at PC
+        
+        self.pc_alu.in1 = self.pc
+        self.pc_alu.add() # pc + 4
 
-        instruction = instruction_memory[pc]
+        self.pc = self.pc_alu.out
+
 
     def instruction_decode(self):
-        pass
+        print(self.instruction.digits(0,31))
+    
+    def cycle(self):
+        self.instruction_fetch()
+        self.instruction_decode()
+
+
+cpu = ARM()
+
+cpu.cycle()
+cpu.cycle()
