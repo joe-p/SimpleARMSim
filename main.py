ae7dc997c772274bbb683a000ad545adddb38955
Polny <polnyj@wit.edu>
2019-07-17 11:43:47 -0400

    Added control and pc class variables

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 
 class ARM:
+
+    control = {
+            "Reg2Loc": 0,
+            "Branch": 0,
+            "MemRead": 0,
+            "MemtoReg": 0,
+            "ALUOp": 0,
+            "MemWrite": 0,
+            "ALUSrc": 0,
+            "RegWrite": 0
+            }
+
+    pc = 0
+
+
     def alu(self):
         pass
 
