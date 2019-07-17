106c5eefd2763e49605c8ae4f521cbb9f4a378cf
Polny <polnyj@wit.edu>
2019-07-17 15:00:21 -0400

    Added MUX class and started to work on updating the register file

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 
+
+class MUX:
+    def __init__(self):
+        self.in0 = 0
+        self.in1 = 0
+        self.select = 0
+
+    def out(self):
+        if self.select == 1:
+            return in1
+        else:
+            return in0
+
 class Binary:
     def __init__(self, n, bits):
         self.bin = '{:0{}b}'.format(n, bits)
@@ -28,10 +41,11 @@ class ALU:
     def __init__(self):
         self.in1 = 0
         self.in2 = 0
-        self.out = 0
+        self.control = 0
 
-    def add(self):
-        self.out = self.in1 + self.in2
+    def out(self):
+        if self.control == 0:
+            return self.in1 + self.in2
 
 class ARM:
 
@@ -49,7 +63,8 @@ class ARM:
     pc = 0
 
     pc_alu = ALU() 
-    mem_alu = ALU()
+
+    reg_mux = MUX()
 
     instruction_memory = {
             0: Binary(0, 32),
@@ -58,7 +73,7 @@ class ARM:
 
     instruction = Binary(0, 32)
 
-    registers = {
+    register_file = {
             "Read Register 1": 0,
             "Read Register 2": 0,
             "Write Register": 0,
@@ -67,6 +82,8 @@ class ARM:
             "Read Data 2": 0
             }
 
+    registers = [0] * 32
+
     def __init__(self):
         self.pc_alu.in2 = 4 # Input to the PC ALU is always 4
 
@@ -74,14 +91,27 @@ class ARM:
         self.instruction = self.instruction_memory[self.pc] # Get the instruction at PC
         
         self.pc_alu.in1 = self.pc
-        self.pc_alu.add() # pc + 4
 
-        self.pc = self.pc_alu.out
+        self.pc = self.pc_alu.out() 
 
 
+    def update_register_file(self):
+        self.register_file["Read Register 1"] = self.instruction.digits(21,31)
+
+        self.reg_mux.in1 = self.instruction.digits(0,4)
+        self.reg_mux.in0 = self.instruction.digits(16,20)
+        self.reg_mux.select = self.control["Reg2Loc"]
+
+        self.register_file["Read Register 2"] = self.reg_mux.out
+        self.register_file["Write Register"] = self.instruction.digits(0,4)
+        # Need to do self.register_file["Write Data"]
+
+  
     def instruction_decode(self):
+        self.update_register_file()
         print(self.instruction.digits(0,31))
-    
+
+        
     def cycle(self):
         self.instruction_fetch()
         self.instruction_decode()
